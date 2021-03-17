1225132ad5c42a0e1be54bfcf28a775845b75f05
jeff5489 <jeff5489@gmail.com>
2021-03-17 14:51:28 -0400

    made message a regular entity, added User entity

--- src/main/java/strunk/entities/Message.java
+++ src/main/java/strunk/entities/Message.java
@@ -2,9 +2,89 @@ package strunk.entities;
 
 import java.sql.Timestamp;
 
-public abstract class Message {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "message")
+public class Message {
+	
+	@Id
+	@GeneratedValue
 	private int id;
 	private int sendingUserId;
 	private int receivingUserId;
 	private Timestamp sentDate;
+	private String originalContent;
+	private String translatedContent;
+	
+	public Message(int sendingUserId, int receivingUserId, Timestamp sentDate, String originalContent,
+			String translatedContent) {
+		super();
+		this.sendingUserId = sendingUserId;
+		this.receivingUserId = receivingUserId;
+		this.sentDate = sentDate;
+		this.originalContent = originalContent;
+		this.translatedContent = translatedContent;
+	}
+
+	public Message() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public int getSendingUserId() {
+		return sendingUserId;
+	}
+
+	public void setSendingUserId(int sendingUserId) {
+		this.sendingUserId = sendingUserId;
+	}
+
+	public int getReceivingUserId() {
+		return receivingUserId;
+	}
+
+	public void setReceivingUserId(int receivingUserId) {
+		this.receivingUserId = receivingUserId;
+	}
+
+	public Timestamp getSentDate() {
+		return sentDate;
+	}
+
+	public void setSentDate(Timestamp sentDate) {
+		this.sentDate = sentDate;
+	}
+
+	public String getOriginalContent() {
+		return originalContent;
+	}
+
+	public void setOriginalContent(String originalContent) {
+		this.originalContent = originalContent;
+	}
+
+	public String getTranslatedContent() {
+		return translatedContent;
+	}
+
+	public void setTranslatedContent(String translatedContent) {
+		this.translatedContent = translatedContent;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", sendingUserId=" + sendingUserId + ", receivingUserId=" + receivingUserId
+				+ ", sentDate=" + sentDate + ", originalContent=" + originalContent + ", translatedContent="
+				+ translatedContent + "]";
+	}
+	
 }
---
+++ src/main/java/strunk/entities/User.java
@@ -0,0 +1,44 @@
+package strunk.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String firstName;
+	
+	public User(int id, String firstName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + "]";
+	}
+	
+}
